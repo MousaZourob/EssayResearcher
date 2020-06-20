a33fbd2be3f961364d7c3e05b542e2353163bf6c
Benson Shen <kyigof@gmail.com>
2020-06-20 19:53:30 -0400

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -2,8 +2,12 @@ if (window.location.href.indexOf('#') > 0) {
     document.getElementById('start').style.display = 'none'
 }
 
+function slideSplash() {
+    $("#start").slideUp("slow");
+}
+
 $(document).ready(function(){
-    $("#start-button").click(function(){
+    $("#start").click(function(){
         $("#start").slideUp("slow");
     });
 });
