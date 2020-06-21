ab326e5bed20bd03cec4d1012efd9bf154ca48ef
Mousa Zourob <mousazourob@gmail.com>
2020-06-21 17:09:07 -0400

    Added links showing up even if excerpt is empty

--- main.py
+++ main.py
@@ -3,54 +3,54 @@ from bs4 import BeautifulSoup
 from requests import get
 
 def get_html_code(url):
-	return get(url).text
+    return get(url).text
 
 def get_google_html(query, keywords):
-	query = query.replace(" ", "+")
-	url = f"https://google.com/search?q={query}"
+    query = query.replace(" ", "+")
+    url = f"https://google.com/search?q={query}"
 
-	if keywords:
-		for word in keywords:
-			url += f"+\"{word}\"+OR"
-		url = url[:-3]
+    if keywords:
+        for word in keywords:
+            url += f"+\"{word}\"+OR"
+        url = url[:-3]
 
-	return get_html_code(url)
+    return get_html_code(url)
 
 def get_paragraphs(url, query, keywords):
-	html_code = get_html_code(url)
-	soup = BeautifulSoup(html_code, "html.parser")
+    html_code = get_html_code(url)
+    soup = BeautifulSoup(html_code, "html.parser")
 
-	for paragraph in soup("p"):
-		paragraph = " ".join(paragraph.get_text().split())
+    for paragraph in soup("p"):
+        paragraph = " ".join(paragraph.get_text().split())
 
-		if not paragraph == "" and (query in paragraph or
-			any(kw in keywords for kw in paragraph)):
+        if not paragraph == "" and (query in paragraph or
+            any(kw in keywords for kw in paragraph)):
 
-			if len(paragraph) > 200:
-				paragraph = paragraph[:200] + " ..."
-				
-			return paragraph
+            if len(paragraph) > 200:
+                paragraph = paragraph[:200] + " ..."
+                
+            return paragraph
 
 def get_top_links(html_code):
-	soup = BeautifulSoup(html_code, "html.parser")
-	avoidLinks = tuple(["youtube.", "google."])
-	links = []
+    soup = BeautifulSoup(html_code, "html.parser")
+    avoidLinks = tuple(["youtube.", "google."])
+    links = []
 
-	for div in soup.find_all("div", class_="BNeawe s3v9rd AP7Wnd"):
-		div.decompose()
+    for div in soup.find_all("div", class_="BNeawe s3v9rd AP7Wnd"):
+        div.decompose()
 
-	for div in soup.find_all("div", class_="kCrYT"):
+    for div in soup.find_all("div", class_="kCrYT"):
 
-		for link in div.find_all("a"):
-			currentLink = link.get("href")
+        for link in div.find_all("a"):
+            currentLink = link.get("href")
 
-			if not any(al in currentLink for al in avoidLinks):
+            if not any(al in currentLink for al in avoidLinks):
 
-				if "&sa=" in currentLink:
-					currentLink = currentLink[7:currentLink.index("&sa=")]
+                if "&sa=" in currentLink:
+                    currentLink = currentLink[7:currentLink.index("&sa=")]
 
-				links.append(currentLink)
-	return links
+                links.append(currentLink)
+    return links
 
 # BS4 Stuff
 ###########################################################################################
