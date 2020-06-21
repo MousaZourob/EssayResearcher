a6f708ac52aa718880e9d4e3a5a466608fe56095
Mousa Zourob <mousazourob@gmail.com>
2020-06-21 12:09:42 -0400

    testing version

--- main.py
+++ main.py
@@ -30,20 +30,26 @@ def get_paragraphs(url, query, keywords):
 	return paragraphs
 
 def get_top_links(html_code):
-	soup = BeautifulSoup(html_code, "html.parser")
-	avoidLinks = tuple(["https://www.youtube", "http://scholar.google"])
-	links = []
+    soup = BeautifulSoup(html_code, "html.parser")
+    avoidLinks = tuple(["youtube.", "google."])
+    links = []
 
-	for div in soup.find_all("div", class_="BNeawe s3v9rd AP7Wnd"):
-		div.decompose()
+    for div in soup.find_all("div", class_="BNeawe s3v9rd AP7Wnd"):
+        div.decompose()
 
-	for div in soup.find_all("div", class_="kCrYT"):
-		for link in div.find_all("a"):
-			currentLink = link.get("href")
-			if "&sa=" in currentLink and not any(al in currentLink for al in avoidLinks):
-				links.append(currentLink[7:currentLink.index("&sa=")])
+    for div in soup.find_all("div", class_="kCrYT"):
 
-	return links
+        for link in div.find_all("a"):
+            currentLink = link.get("href")
+
+            if not any(al in currentLink for al in avoidLinks):
+
+                if "&sa=" in currentLink:
+                    currentLink = currentLink[7:currentLink.index("&sa=")]
+
+                links.append(currentLink)
+
+    return links
 
 # BS4 Stuff
 ###########################################################################################
@@ -59,15 +65,11 @@ temp = "#"
 def home():                                 # Defines and renders home page
 	if request.method == "POST":
 		user_input = []
-		paragraphs = []
-
+		
 		topic = request.form["research"]
 		user_input = request.form["keywords"].split(", ")
 
-		links = get_top_links(get_google_html(topic, user_input))
-
-		for link in links:
-			paragraphs.append(get_paragraphs(link, topic, user_input))
+		user_input.insert(0, topic)
 
 		check_empty = False
 		for word in user_input:
@@ -78,16 +80,31 @@ def home():                                 # Defines and renders home page
 			flash("Fill in all the boxes!")
 			return redirect(url_for("home"))
 
-		return redirect(url_for("research", words=["12", "34", "56"]))
+		return redirect(url_for("research", words=user_input))
 	else: 
 		return render_template("home.html")
 
 @app.route("/<words>")                                      # Sets URL tag for research page
 def research(words):                                        # Defines and renders research page
 	paragraphs = []
-	paragraphs = words[1:len(words)-1].split(", ")			# Turns words from a string to a list to be printed
+	input = []
+	input = words[1:len(words)-1].split(", ")				# Turns words from a string to a list to be printed
 
-	return render_template("research.html", content=["test1", "test2", "test3"])	# Renders website
+	print("test")
+
+	topic = input[0]
+	keywords = input[1:len(input)-1]
+
+	links = get_top_links(get_google_html(topic, keywords))
+
+	for link in links:
+		paragraphs.append(get_paragraphs(link, topic, keywords))
+		print(link)
+
+	print("test")
+	print(paragraphs)
+
+	return render_template("research.html", content=paragraphs)	# Renders website
 	
 if __name__ == "__main__":  # Runs website
 	app.run(debug=True)
