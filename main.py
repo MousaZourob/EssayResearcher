032ac99bbb104eb073cc8dc24304305435e500ed
Jun Zheng <56985601+JunZheng-dev@users.noreply.github.com>
2020-06-21 18:16:03 -0400

    Add files via upload
    added choices

--- main.py
+++ main.py
@@ -3,54 +3,74 @@ from bs4 import BeautifulSoup
 from requests import get
 
 def get_html_code(url):
-    return get(url).text
+	return get(url).text
 
 def get_google_html(query, keywords):
-    query = query.replace(" ", "+")
-    url = f"https://google.com/search?q={query}"
+	query = query.replace(" ", "+")
+	url = f"https://google.com/search?q={query}"
 
-    if keywords:
-        for word in keywords:
-            url += f"+\"{word}\"+OR"
-        url = url[:-3]
+	if keywords:
+		for word in keywords:
+			url += f"+\"{word}\"+OR"
+		url = url[:-3]
 
-    return get_html_code(url)
+	return get_html_code(url)
 
 def get_paragraphs(url, query, keywords):
-    html_code = get_html_code(url)
-    soup = BeautifulSoup(html_code, "html.parser")
+	html_code = get_html_code(url)
+	soup = BeautifulSoup(html_code, "html.parser")
 
-    for paragraph in soup("p"):
-        paragraph = " ".join(paragraph.get_text().split())
+	for paragraph in soup("p"):
+		paragraph = " ".join(paragraph.get_text().split())
 
-        if not paragraph == "" and (query in paragraph or
-            any(kw in keywords for kw in paragraph)):
+		if not paragraph == "" and (query in paragraph or
+			any(kw in keywords for kw in paragraph)):
 
-            if len(paragraph) > 200:
-                paragraph = paragraph[:200] + " ..."
-                
-            return paragraph
+			if len(paragraph) > 200:
+				paragraph = paragraph[:200] + " ..."
+				
+			return paragraph
 
-def get_top_links(html_code):
-    soup = BeautifulSoup(html_code, "html.parser")
-    avoidLinks = tuple(["youtube.", "google."])
-    links = []
+def get_top_links(html_code, choices):
+	soup = BeautifulSoup(html_code, "html.parser")
+	avoidLinks = tuple(["youtube.", "google."])
+	links = []
+	suffixChoices = tuple([".edu", ".org", ".gov"])
 
-    for div in soup.find_all("div", class_="BNeawe s3v9rd AP7Wnd"):
-        div.decompose()
+	for div in soup.find_all("div", class_="BNeawe s3v9rd AP7Wnd"):
+		div.decompose()
 
-    for div in soup.find_all("div", class_="kCrYT"):
+	choices = choices[1:-1]
+	choices = choices.split(", ")
 
-        for link in div.find_all("a"):
-            currentLink = link.get("href")
+	if "True" in choices:
+		for div in soup.find_all("div", class_="kCrYT"):
 
-            if not any(al in currentLink for al in avoidLinks):
+			for link in div.find_all("a"):
+				currentLink = link.get("href")
 
-                if "&sa=" in currentLink:
-                    currentLink = currentLink[7:currentLink.index("&sa=")]
+				for i in range(len(choices)):
+					if choices[i] == "True":
+						if suffixChoices[i] in currentLink:
+							if not any(al in currentLink for al in avoidLinks):
 
-                links.append(currentLink)
-    return links
+								if "&sa=" in currentLink:
+									currentLink = currentLink[7:currentLink.index("&sa=")]
+
+								links.append(currentLink)
+	else:
+		for div in soup.find_all("div", class_="kCrYT"):
+
+			for link in div.find_all("a"):
+				currentLink = link.get("href")
+
+				if not any(al in currentLink for al in avoidLinks):
+
+					if "&sa=" in currentLink:
+						currentLink = currentLink[7:currentLink.index("&sa=")]
+
+					links.append(currentLink)
+	return links
 
 # BS4 Stuff
 ###########################################################################################
@@ -59,7 +79,6 @@ def get_top_links(html_code):
 app = Flask(__name__)   # Initializes app instance 
 app.secret_key = "0RjiQhdtLs"
 temp = "#"
-
 
 @app.route("/", methods=["POST", "GET"])    # Sets URL tag for home page and initializes HTTP protocols 
 @app.route("/#", methods=["POST", "GET"]) 	# Second route to not display landing page twice
@@ -74,6 +93,12 @@ def home():                                 # Defines and renders home page
 		choices_list.append(request.form.getlist('option1'))	# Gets extra options
 		choices_list.append(request.form.getlist('option2'))	
 		choices_list.append(request.form.getlist('option3'))	
+
+		for i in range(len(choices_list)):
+			if (choices_list[i] == ["on"]):
+				choices_list[i] = True
+			else:
+				choices_list[i] = False
 
 		user_input.insert(0, topic)		# Combines topic and keywords into 1 list
 
@@ -101,7 +126,7 @@ def research(words, choices):                                         # Defines 
 	keywords = temp[1:]		# Gets keywords from temp list
 
 	code_html = get_google_html(topic, keywords)	# Gets HTML code
-	links = get_top_links(code_html)				# Sends HTML code to get top links
+	links = get_top_links(code_html, choices)		# Sends HTML code to get top links
 
 	for i in range(len(links)):		# Gets paragraphs using links
 		paragraphs.append(get_paragraphs(links[i], topic, keywords))	# Adds paragraphs to paragraphs list
